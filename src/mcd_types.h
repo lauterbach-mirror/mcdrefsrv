9c7a050de6341d841d7d1330acb40662e79fa11a
Nicolas Eder <nicolas.eder@lauterbach.com>
2023-11-22 13:04:30 +0100

    mcd api header files updated to lauterbach internal verison 1.1

--- src/mcd_types.h
+++ src/mcd_types.h
@@ -1,63 +1,65 @@
 /** \file mcd_types.h
-    \brief The Multi-core Debug (MCD) API basic types
+	\brief The Multi-core Debug (MCD) API basic types
 
-    This is the definition of basic scalar types used by the SPRINT API.
-    
-    New host platforms may require changes in this file.
+	This is the definition of basic scalar types used by the Lauterbach version of the SPRINT API.
+	In contrast to the SPRINT MCD API version 1.0 boolean types and char types are here defined with
+	a size, which is fixed for the same platform.
+
+	New host platforms may require changes in this file.
 */
-
 
 #ifndef __mcd_types_h
 #define __mcd_types_h
 
-#ifdef _MSC_VER
-typedef __int8            int8_t;
-typedef __int16           int16_t;
-typedef __int32           int32_t;
-typedef __int64           int64_t;
-typedef unsigned __int8   uint8_t;
-typedef unsigned __int16  uint16_t;
-typedef unsigned __int32  uint32_t;
-typedef unsigned __int64  uint64_t;
+
+#if defined(_MSC_VER) && (_MSC_VER<1600)
+  typedef __int8            int8_t;
+  typedef __int16           int16_t;
+  typedef __int32           int32_t;
+  typedef __int64           int64_t;
+  typedef unsigned __int8   uint8_t;
+  typedef unsigned __int16  uint16_t;
+  typedef unsigned __int32  uint32_t;
+  typedef unsigned __int64  uint64_t;
 #else
-#if defined(__SUNPRO_C) || defined(__hpux)
-#include <inttypes.h>
+# if defined(__SUNPRO_C) || defined(__SUNPRO_CC) || defined(__hpux)
+#   include <inttypes.h>
+# else
+#   include <stdint.h>
+# endif
+#endif
+
+
+/* Definition of character type (mcd_char_t):
+ * Characters are only used for strings.
+ * We assume here that "char" has the same size for all compilers on a platform.
+ * The width of a character should correspond to the size of a minimum addressable unit.
+ * So for byte addressable machines the mcd_char_t should have the width of 8-bits.
+ */
+typedef char mcd_char_t;
+
+
+/* Definition of the boolean type (mcd_bool_t):
+ * The boolean types must have always the same size with any compiler on the same platform.
+ * Booleans are here defined with 8-bits or 32-bits depending on the used platform to have optimal compatibility
+ * with the original SPRINT MCD API 1.0.
+ * -> On Linux and MacOS X operating systems booleans must have 8-bits
+ * -> On all other platforms booleans must have 32-bits (stored in target Endianess)
+ * Note, that both C and C++ standard define their boolean type (bool or _Bool) not with a fixed size.
+ */
+#if defined(__linux__) || defined(__APPLE__)
+typedef uint8_t  mcd_bool_t;
 #else
-#include <stdint.h>
-#endif
-#endif
-
-/* Replacement for stdbool.h */
-#ifndef __cplusplus
-
-#if defined(__GNUC__) && __GNUC__ >= 3
-
-#undef	bool
-#undef	true
-#undef	false
-
-/* Support for _C99: type _Bool is already built-in. */
-#define false	0
-#define true	1
-
-/* User visible type `bool' is provided as a macro which may be redefined */
-#define bool _Bool
-
-#else
-
-/* `bool' type must promote to `int' or `unsigned int'. */
-typedef enum {
-	false = 0,
-	true = 1
-} bool;
-
+typedef uint32_t mcd_bool_t;
 #endif
 
-#endif /* __cplusplus */
-
-/* Inform that everything is fine */
-#define __bool_true_false_are_defined 1
-
+#if !defined(FALSE)
+# define FALSE 0
+#endif
+#if !defined(TRUE)
+# define TRUE  1
 #endif
 
+
+#endif /* __mcd_types_h */
 
